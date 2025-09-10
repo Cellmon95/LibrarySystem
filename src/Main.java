bb671416becfc6793042901f364bea71702a1d9c
Lucas Källberg <lucas.arne@hotmail.com>
2025-09-10 14:59:36 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // I main-metoden - ENDAST dessa listor för att spara tid
+        // Böcker (index motsvarar varandra)
+        ArrayList<String> bookTitles = new ArrayList<>();
+        ArrayList<String> bookAuthors = new ArrayList<>();
+        ArrayList<String> bookISBN = new ArrayList<>();
+        ArrayList<Boolean> bookAvailable = new ArrayList<>(); // true =
+
+        // Lån (index motsvarar varandra)
+        ArrayList<String> borrowerNames = new ArrayList<>();
+        ArrayList<String> borrowedBooks = new ArrayList<>(); // ISBN för lånad bok
+
+        // Användare (index motsvarar varandra) Förinställd testdata (Person 5 skapar detta) Enkel huvudmeny (Person 5)
+        ArrayList<String> userNames = new ArrayList<>();
+        ArrayList<String> phoneNumbers = new ArrayList<>();
+
+    }
+}
