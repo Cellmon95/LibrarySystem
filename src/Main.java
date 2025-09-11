ef68190a006ac198ad12763ae4aae5198b19b5bc
khaled87 <khaled.nawasreh@gmail.com>
2025-09-11 10:43:12 +0200

    added methods

--- src/Main.java
+++ src/Main.java
@@ -20,4 +20,39 @@ public class Main {
         ArrayList<String> phoneNumbers = new ArrayList<>();
 
     }
+
+    public static void addBook(ArrayList<String> titles,
+                               ArrayList<String> authors, ArrayList<String> isbn, String
+                                       title, String author, String isbnNumber) {
+        titles.add(title);
+        authors.add(author);
+        isbn.add(isbnNumber);
+
+        System.out.println("Book added: " + title + "by " + author + "ISBN Number: " + isbnNumber);
+    }
+
+    public static void displayAllBooks(ArrayList<String> titles,
+                                       ArrayList<String> authors, ArrayList<String> isbn) {
+
+        if(titles.isEmpty()){
+            System.out.println("No book available.");
+        }
+
+        for(int i = 0; i<= titles.size(); i++){
+            System.out.println(titles.get(i) +  "by " + authors.get(i) + "ISBN Number: " + isbn.get(i));
+        }
+    }
+
+    public static int searchBook(ArrayList<String> titles,
+                                 ArrayList<String> authors, String searchTerm) {
+
+        for (int i = 0; i < titles.size(); i++) {
+            if (titles.get(i).toLowerCase().contains(searchTerm.toLowerCase()) ||
+                    authors.get(i).toLowerCase().contains(searchTerm.toLowerCase())) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
 }
